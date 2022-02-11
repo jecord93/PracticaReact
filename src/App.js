dd76cf33567c7100ec3dfaa9d601af0b73ac128e
Jesus <jesuscord93@gmail.com>
2022-02-11 17:13:52 -0500

    Practica de React 1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+
+function HelloWorld(props) {
+  return (
+    <div id="hello">
+      <h1>{props.myText}</h1>
+    </div>
+  );
+
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>This is my component: 
+      <HelloWorld myText="Hello PC"/> 
+      <HelloWorld myText="¿Cómo estás?"/> 
     </div>
+
   );
 }
 
